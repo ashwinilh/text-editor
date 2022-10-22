5e4621fe63e874fc4b6ccfa6014b1b17293027d5
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-22 16:02:58 -0500

    updating webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new WebpackPwaManifest({
+        name: 'Just another Text Editor Application',
+        short_name: 'JATE',
+        description: 'Take notes!',
+        start_url: './',
+        publicPath: './',
+        background_color: '#6495ED',
+        theme_color: '#6495ED',
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: '1024x1024',
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable'
+          },
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        favicon: './favicon.ico',
+        title: 'Webpack Plugin',
+      })
+
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource',
+        }
       ],
     },
   };
